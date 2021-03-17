d0436b15936ce795d34ad694a1ca869e9db55be7
rodrigoptu12 <rod@Rodrigo.localdomain>
2021-03-17 17:40:18 -0300

    Adicionando arquivos

---
+++ source/romano.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+// Copyright 2021
+#include <string.h>
+#include <romano.h>
+int main() {
+    int a[100], T , i , j , k;
+    char rom[100];
+    printf("\nDigite um numeral Romano: ");
+    scanf("%s", &rom);
+
+}
